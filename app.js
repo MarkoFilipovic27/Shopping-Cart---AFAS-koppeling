9eed77fbe43a9430d6d409b1b45745c1d2d44946
MarkoFilipovic27 <markofilipo@hotmail.com>
2022-08-04 12:34:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-console.log(fetch('https://31219.restaccept.afas.online/ProfitRestServices/connectors/Items?skip=0&take=100'));
+fetch('https://31219.restaccept.afas.online/ProfitRestServices/connectors/Items?skip=0&take=100', {
+    method: 'get',
+    headers: {
+        Authorization: "AfasToken " + window.btoa("<token><version>1</version><data>5BABD7C96D94488E9A2CB1C19B8CBD2DC23A01ED428F63F81FC1A7ABB6DE57F4</data></token>")
+    }
 
-
-<token><version>1</version><data>5BABD7C96D94488E9A2CB1C19B8CBD2DC23A01ED428F63F81FC1A7ABB6DE57F4</data></token>
+})
+.then(response => response.json())
+.then(json => console.log(json))
