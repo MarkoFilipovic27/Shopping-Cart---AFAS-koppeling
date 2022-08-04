58ac57508002e1f09ec6a4ef26f75578f64a7ab1
MarkoFilipovic27 <markofilipo@hotmail.com>
2022-08-04 12:21:44 +0200

    11
    asd

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log(fetch('https://31219.restaccept.afas.online/ProfitRestServices/connectors/Items?skip=0&take=100'));
+
+
+<token><version>1</version><data>5BABD7C96D94488E9A2CB1C19B8CBD2DC23A01ED428F63F81FC1A7ABB6DE57F4</data></token>
