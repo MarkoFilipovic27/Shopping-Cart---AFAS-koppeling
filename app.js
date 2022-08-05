d3f0ee69f9609078743a0ce6e6db8b09addca0a4
MarkoFilipovic27 <markofilipo@hotmail.com>
2022-08-05 11:31:34 +0200

    11

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ fetch('https://31219.restaccept.afas.online/ProfitRestServices/connectors/Items?
         Authorization: "AfasToken " + window.btoa("<token><version>1</version><data>5BABD7C96D94488E9A2CB1C19B8CBD2DC23A01ED428F63F81FC1A7ABB6DE57F4</data></token>")
     }
 
+
 })
 .then(response => response.json())
 .then(json => console.log(json))};
+
 
 function createItemdiv(){
     const newDiv = document.createElement("div");
